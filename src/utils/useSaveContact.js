5cb2c1e95af5e46597335e7930be5c5095bbcd13
Abdallah Fiasal123 <abdallah.fisal@gmail.com>
2026-03-12 05:24:42 +0200

    test3

--- src/utils/useSaveContact.js
+++ src/utils/useSaveContact.js
@@ -8,24 +8,12 @@ export const saveContact = async (currentUserId, currentUserData, otherUser, cha
   try {
     // 1. حفظ في قائمة جهات اتصالي (أنا)
     const myContactRef = doc(db, "users", currentUserId, "myContacts", otherUser._id);
-    await setDoc(myContactRef, {
-      userId: otherUser._id,
-      username: otherUser.name || otherUser.username,
-      photo: otherUser.photo || "",
-      chatId: chatId,
-      lastInteraction: new Date() 
-    }, { merge: true });
+    await setDoc(doc(db, "users", myId, "myContacts", otherId), data);
 
     // 2. حفظ في قائمة جهات اتصال الطرف التاني (هو)
     // السحر هنا: بنسجل بياناتي أنا عنده عشان يظهرله الشات فوراً
     const otherContactRef = doc(db, "users", otherUser._id, "myContacts", currentUserId);
-    await setDoc(otherContactRef, {
-      userId: currentUserId,
-      username: currentUserData.name, // اسمك أنت
-      photo: currentUserData.photo || "", // صورتك أنت
-      chatId: chatId,
-      lastInteraction: new Date()
-    }, { merge: true });
+    await setDoc(doc(db, "users", otherId, "myContacts", myId), data);
 
     console.log("Contact saved for both sides!");
   } catch (e) {
